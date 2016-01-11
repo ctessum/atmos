83af15d9f6517d819aa44ef874764b98d98ac5dc
Chris Tessum <ctessum@gmail.com>
2016-01-11 14:45:49 -0600

    Change to evalstats

--- evalstats/stats.go
+++ evalstats/stats.go
@@ -1,8 +1,10 @@
-package modeleval
+// Package evalstats calculates metrics for evaluating physical models.
+package evalstats
 
 import "math"
 
-// MFB calculates the mean fractional bias of a against b.
+// MFB calculates the mean fractional bias of b against a. It assumes a and
+// b are the same length.
 func MFB(a, b []float64) float64 {
 	r := 0.
 	for i, v1 := range a {
@@ -12,7 +14,8 @@ func MFB(a, b []float64) float64 {
 	return r / float64(len(a))
 }
 
-// MFE calculates the mean fractional error of a against b.
+// MFE calculates the mean fractional error of b against a. It assumes a and
+// b are the same length.
 func MFE(a, b []float64) float64 {
 	r := 0.
 	for i, v1 := range a {
@@ -22,7 +25,8 @@ func MFE(a, b []float64) float64 {
 	return r / float64(len(a))
 }
 
-// MB calculates the mean bias of a against b.
+// MB calculates the mean bias of b against a. It assumes a and
+// b are the same length.
 func MB(a, b []float64) float64 {
 	r := 0.
 	for i, v1 := range a {
@@ -32,12 +36,23 @@ func MB(a, b []float64) float64 {
 	return r / float64(len(a))
 }
 
-// ME calculates the mean error of a against b.
+// ME calculates the mean error of b against a. It assumes a and
+// b are the same length.
 func ME(a, b []float64) float64 {
 	r := 0.
 	for i, v1 := range a {
 		v2 := b[i]
 		r += math.Abs(v2 - v1)
+	}
+	return r / float64(len(a))
+}
+
+// MR calculates the mean ratio of b:a. It assumes a and
+// b are the same length.
+func MR(a, b []float64) float64 {
+	r := 0.
+	for i, v1 := range a {
+		r += b[i] / v1
 	}
 	return r / float64(len(a))
 }
